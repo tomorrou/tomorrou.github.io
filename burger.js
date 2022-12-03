257ddb4248a59ff36992b36bc8275b0eddc26e26
tomorrou <rudneva07@bk.ru>
2022-12-03 23:26:28 +0300

    delete everything (again...)

--- burger.js
+++
@@ -1,9 +0,0 @@
-var burger_button = document.getElementById('burger');
-var menu = document.getElementById('menu-list');
-
-burger_button.addEventListener('mousedown', activate_menu);
-
-function activate_menu() {
-    menu.classList.toggle('active');
-    burger_button.classList.toggle('active');
-}
